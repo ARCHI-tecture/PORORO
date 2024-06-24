3e3e379e750ac35501e50f9a41335a2de4398a32
unknown <hc0606@gmail.com>
2024-06-24 11:55:31 +0900

    palette

--- Frontend/src/index.tsx
+++ Frontend/src/index.tsx
@@ -3,14 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+4;
+import { createTheme, PaletteOptions } from '@mui/material/styles';
+import { CustomPalette } from './components/type';
+
+declare module '@mui/material/styles' {
+  interface Palette {
+    customPalette: CustomPalette;
+  }
+  interface PaletteOptions {
+    customPalette?: CustomPalette;
+  }
+}
+
+const theme = createTheme({
+  palette: {
+    customPalette: {
+      mainColor: { main: '#FF7237' },
+      subColor1: { main: '#FBF6F4' },
+      subColor2: { main: '#FF230F' },
+      subColor3: { main: '#654039' },
+      subColor4: { main: '#817F82' },
+    },
+  } as PaletteOptions,
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
